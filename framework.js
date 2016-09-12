ce3ea9318e1236e44c3da1aac95a8370debe7af7
Kevin Xi <kevin.xgr@gmail.com>
2016-09-12 23:41:30 +0800

    Add draft for core structure.

--- framework.js
+++ framework.js
@@ -1,20 +1,37 @@
 'use strict';
+const net = require('net');
+const socket = net.Socket;
+
 // TODO: use traditional way or ?
 let Framework = {
     inject: function (coreName) {
+        this.core = getCore(coreName);
         console.log(`framework inject ${coreName}`);
-        // checkCore();
-        this.coreName = coreName;
         return this;
     },
 
-    start: function () { console.log(`server start with ${this.coreName}`); },
+    start: function () {
+        const server = net.createServer(onConnect);
+        server.listen(this.core.system.port, () => {
+            console.log(`server start with ${this.core.name}`);
+        });
+    },
 
     // internal
-    coreName: '',
-    checkCore: (coreName) => {
-
-    }
+    core: {}
 };
+
+function onConnect() {
+
+}
+
+function getCore(coreName) {
+    return {
+        name: coreName,
+        system: {
+            port: 9922
+        }
+    };
+}
 
 module.exports = Framework;
